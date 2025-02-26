5c48c23f138ef337b6dbef936e7d5eb6d3dc8701
Jayant <jayantmehra02@gmail.com>
2025-02-26 16:41:19 +0000

    dark mode color fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,23 +7,23 @@ export const Header = () => {
   return (
     <header className="bg-neutral-700 flex items-center gap-x-3">
       <button className="p-4 bg-neutral-600">
-        <MenuIcon className="size-6" />
+        <MenuIcon className="size-6 text-white" />
       </button>
       <div className="flex items-center justify-between w-full p-2">
         <div className="flex items-center gap-x-2">
-          <DocumentIcon className="size-5" />
+          <DocumentIcon className="size-5 text-white" />
           <div className="flex flex-col">
             <span className="body-m text-neutral-400 hidden md:inline-block">
               Document Name
             </span>
-            <span className="heading-m">welcome.md</span>
+            <span className="heading-m text-white">welcome.md</span>
           </div>
         </div>
         <div className="flex items-center gap-x-4">
           <button>
             <DeleteIcon className="size-5 text-neutral-400" />
           </button>
-          <button className="bg-primary p-2.5 md:size-fit flex items-center justify-center rounded-md md:px-4 md:py-2">
+          <button className="bg-primary text-primary-foreground p-2.5 md:size-fit flex items-center justify-center rounded-md md:px-4 md:py-2">
             <SaveIcon className="size-5 " />
             <span className="heading-m hidden md:inline-block">
               Save Changes
