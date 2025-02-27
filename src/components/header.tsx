ac4fa2e14de23f5bee4f537cbf10e676311c5dec
Jayant <jayantmehra02@gmail.com>
2025-02-27 15:42:05 +0000

    logo added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,8 @@ import { DocumentIcon } from "@/components/icons/document-icon";
 import { MenuIcon } from "@/components/icons/menu-icon";
 import { SaveIcon } from "@/components/icons/save-icon";
 
+import { Logo } from "./icons/logo";
+
 export const Header = () => {
   const [documentName, setDocumentName] = useState("untitled.md");
   const [isEditing, setIsEditing] = useState(false);
@@ -16,6 +18,10 @@ export const Header = () => {
       <button className="py-4 px-3 bg-neutral-600 hover:bg-primary cursor-pointer transition-colors duration-150">
         <MenuIcon className="text-white" />
       </button>
+      <div className="mx-2 hidden lg:block text-white">
+        <Logo />
+      </div>
+      <div className="w-px h-[2rem] bg-neutral-500 hidden lg:block" />
       <div className="flex items-center justify-between w-full p-2">
         <div className="flex items-center gap-x-2">
           <DocumentIcon className="size-5 text-white" />
@@ -46,7 +52,7 @@ export const Header = () => {
           <button className="text-neutral-400 hover:text-primary transition-colors duration-150 cursor-pointer">
             <DeleteIcon className="size-5" />
           </button>
-          <button className="bg-primary text-primary-foreground p-2.5 md:size-fit flex items-center justify-center rounded-md md:px-4 md:py-2 hover:bg-primary-hover transition-colors duration-150 cursor-pointer">
+          <button className="bg-primary text-primary-foreground p-2.5 md:size-fit flex items-center justify-center gap-x-2 rounded-md md:px-4 md:py-2 hover:bg-primary-hover transition-colors duration-150 cursor-pointer">
             <SaveIcon />
             <span className="heading-m hidden md:inline-block">
               Save Changes
