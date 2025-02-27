d6eec570e4fa4c652ab94c75c7a8cb6f98999c7f
Jayant <jayantmehra02@gmail.com>
2025-02-27 15:36:46 +0000

    header active state changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
 import { DeleteIcon } from "@/components/icons/delete-icon";
 import { DocumentIcon } from "@/components/icons/document-icon";
 import { MenuIcon } from "@/components/icons/menu-icon";
 import { SaveIcon } from "@/components/icons/save-icon";
 
 export const Header = () => {
+  const [documentName, setDocumentName] = useState("untitled.md");
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <header className="bg-neutral-700 flex items-center gap-x-3">
-      <button className="p-4 bg-neutral-600">
-        <MenuIcon className="size-6 text-white" />
+      <button className="py-4 px-3 bg-neutral-600 hover:bg-primary cursor-pointer transition-colors duration-150">
+        <MenuIcon className="text-white" />
       </button>
       <div className="flex items-center justify-between w-full p-2">
         <div className="flex items-center gap-x-2">
@@ -16,15 +23,31 @@ export const Header = () => {
             <span className="body-m text-neutral-400 hidden md:inline-block">
               Document Name
             </span>
-            <span className="heading-m text-white">welcome.md</span>
+            {isEditing ? (
+              <input
+                type="text"
+                className={`heading-m text-white py-1 pr-2 outline-none border-b caret-primary`}
+                value={documentName}
+                onChange={(e) => setDocumentName(e.target.value)}
+                onBlur={() => setIsEditing(false)}
+                autoFocus
+              />
+            ) : (
+              <span
+                className="heading-m text-white border-b border-transparent"
+                onClick={() => setIsEditing(true)}
+              >
+                {documentName}
+              </span>
+            )}
           </div>
         </div>
-        <div className="flex items-center gap-x-4">
-          <button>
-            <DeleteIcon className="size-5 text-neutral-400" />
+        <div className="flex items-center gap-x-4 ">
+          <button className="text-neutral-400 hover:text-primary transition-colors duration-150 cursor-pointer">
+            <DeleteIcon className="size-5" />
           </button>
-          <button className="bg-primary text-primary-foreground p-2.5 md:size-fit flex items-center justify-center rounded-md md:px-4 md:py-2">
-            <SaveIcon className="size-5 " />
+          <button className="bg-primary text-primary-foreground p-2.5 md:size-fit flex items-center justify-center rounded-md md:px-4 md:py-2 hover:bg-primary-hover transition-colors duration-150 cursor-pointer">
+            <SaveIcon />
             <span className="heading-m hidden md:inline-block">
               Save Changes
             </span>
